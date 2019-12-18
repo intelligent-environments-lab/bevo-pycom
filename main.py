58792f9b7e28bed474fb76d9930c2466978fa95a
tungto97@gmail.com <tungto97@gmail.com>
2019-12-17 22:47:54 -0500

    co2 sensor added, untested

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-from sps30 import sps30
 import _thread
 import time
+from sps30 import sps30
+from scd30 import scd30
 
 def main():
+
+    # Sensor initializations
     pm_sensor = sps30()
+    co2_sensor = scd30()
 
     # Start in a separate thread
     pm_thread = _thread.start_new_thread(pm_sensor.start, ())
+    co2_thread = _thread.start_new_thread(co2_sensor.start, ())
 
     # DEBUGGING
-    print("fan started!")
+    print("Sensors running for 5 seconds")
     time.sleep(5)
 
-    # Thread ends naturally
+    # End threads
     pm_sensor.stop()
+    co2_sensor.stop()
 
 if __name__ == "__main__":
     main()
--- scd30.py
+++ scd30.py
@@ -3,7 +3,7 @@
 # class methods:
 # - start()
 # - stop()
-# 
+#
 # start() should be run from a separate thread so that stop() can update exit flag
 
 from machine import I2C
@@ -12,23 +12,23 @@ import time
 import struct
 from crc8 import calc_crc8
 
-# SPS30 I2C ID
-SPS30_I2C_ID = 0x69
+# SCD30 I2C ID
+SCD30_I2C_ID = 0x61
 
-# SPS30 I2C COMMANDS
-SPS30_START_ADDR    = b'\x00\x10'
-SPS30_READY_ADDR    = b'\x02\x02'
-SPS30_READ_ADDR     = b'\x03\x00'
-SPS30_STOP_ADDR     = b'\x01\x04'
+# SCD30 I2C COMMANDS
+SCD30_START_ADDR    = b'\x00\x10'
+SCD30_READY_ADDR    = b'\x02\x02'
+SCD30_READ_ADDR     = b'\x03\x00'
+SCD30_STOP_ADDR     = b'\x01\x04'
 
 # SPS I2C COMMAND OPTIONS
-SPS30_START_MEASUREMENT = b'\x03\x00'
+SCD30_START_MEASUREMENT = b'\x03\x00'
 
-# SPS30_1 PINS
-SPS30_1_SDA = 'P10'
-SPS30_1_SCL = 'P11'
+# SCD30_1 PINS
+SCD30_1_SDA = 'P10'
+SCD30_1_SCL = 'P11'
 
-class sps30:
+class scd30:
 
     _i2c = None
     _interval = None
@@ -36,11 +36,11 @@ class sps30:
 
     _exit_flag = False
 
-    def __init__(self, b=1, p=(SPS30_1_SDA, SPS30_1_SCL), br=20000, interval=10):
+    def __init__(self, b=1, p=(SCD30_1_SDA, SCD30_1_SCL), br=20000, interval=10):
 
         self._interval = interval
         self._i2c = I2C(b, pins=p, baudrate=br)
-        assert SPS30_I2C_ID in self._i2c.scan(), "SPS30 not connected."
+        assert SCD30_I2C_ID in self._i2c.scan(), "SCD30 not connected."
 
         self._send_start()
 
@@ -54,11 +54,11 @@ class sps30:
             while not(self._is_ready()):
                 time.sleep(0.1)
 
-            # Sample sps30
+            # Sample SCD30
             read = self._read_data()
 
             # Check crc8 and deserialize
-            for i in range(0, 10):
+            for i in range(0, 3):
 
                 # crc8 check
                 assert calc_crc8(read[i * 6 : i * 6 + 2]) == bytes([read[i * 6 + 2]]), "Bad upper crc8"
@@ -74,34 +74,31 @@ class sps30:
     # Called in a separate thread
     def stop(self):
 
-        self._i2c.writeto(SPS30_I2C_ID, SPS30_STOP_ADDR)
+        self._i2c.writeto(SCD30_I2C_ID, SCD30_STOP_ADDR)
         self._i2c.deinit()
         self._exit_flag = True
 
     def _send_start(self):
 
         # Start measurement mode, returns number of bytes written
-        return self._i2c.writeto(SPS30_I2C_ID, SPS30_START_ADDR + SPS30_START_MEASUREMENT + calc_crc8(SPS30_START_MEASUREMENT))
+        return self._i2c.writeto(SCD30_I2C_ID, SCD30_START_ADDR + SCD30_START_MEASUREMENT + calc_crc8(SCD30_START_MEASUREMENT))
 
     def _is_ready(self):
 
         # Returns device ready flag
-        self._i2c.writeto(SPS30_I2C_ID, SPS30_READY_ADDR)
-        read = self._i2c.readfrom(SPS30_I2C_ID, 3)
+        self._i2c.writeto(SCD30_I2C_ID, SCD30_READY_ADDR)
+        read = self._i2c.readfrom(SCD30_I2C_ID, 3)
 
         return bytes([read[2]]) == calc_crc8(read[0:2])
 
     def _read_data(self):
 
         # Returns read data
-        self._i2c.writeto(SPS30_I2C_ID, SPS30_READ_ADDR)
-        return self._i2c.readfrom(SPS30_I2C_ID, 60)
+        self._i2c.writeto(SCD30_I2C_ID, SCD30_READ_ADDR)
+        return self._i2c.readfrom(SCD30_I2C_ID, 60)
 
     # DEBUGGING ONLY, SHOULD LOG DATA
     def _print(self):
-        print('PM 0.5 conc.: {} cm-3'.format(_curr_data[4]))
-        print('PM 1.0 conc.: {} cm-3\tMass conc.: {} ug/m3'.format(_curr_data[5], _curr_data[0]))
-        print('PM 2.5 conc.: {} cm-3\tMass conc.: {} ug/m3'.format(_curr_data[6], _curr_data[1]))
-        print('PM 4.0 conc.: {} cm-3\tMass conc.: {} ug/m3'.format(_curr_data[7], _curr_data[2]))
-        print('PM 10. conc.: {} cm-3\tMass conc.: {} ug/m3'.format(_curr_data[8], _curr_data[3]))
-        print('Typical particle size: {} um'.format(_curr_data[9]))
+        print('CO2 conc.: {} ppm'.format(self._curr_data[0]))
+        print('Temperature: {} C'.format(self._curr_data[1]))
+        print('Humidity: {} %'.format(self._curr_data[2]))
